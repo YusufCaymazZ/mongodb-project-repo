b2763fb306de15a9c16a508de5c1c7241f8d3f8c
YusufCaymazZ <yuscaymaz@gmail.com>
2024-01-13 12:57:28 +0300

    i ve just started new

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-import mongodb
-import pandas as pd 
-import matplotlib.pyplot as plt
-
 import os
+import datetime
+from PIL import Image
+
+from gridfs import GridFS
+from pymongo import MongoClient
+import pymongo
+from bson import ObjectId
+import pprint
+from pymongo.errors import OperationFailure
+
+import pandas 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+load_dotenv()
+
+
+
+
+class Addition:
+    def __init__(self,path = None):
+        self.path = path
+
+    def Addition(self):
+        pass
+
+
+
